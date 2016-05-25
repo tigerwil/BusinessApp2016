8546889dc30f791e1089a6668ed210aad4e7042d
Marc Williams <oti_instructor@hotmail.com>
2016-05-25 09:33:25 -0300

    getDepartments

--- BusinessApp2016/www/scripts/index.js
+++ BusinessApp2016/www/scripts/index.js
@@ -9,8 +9,10 @@
     },
     onDeviceReady: function () {
         //Open SQLite Database
+        myDB = window.sqlitePlugin.openDatabase({ name: "business.db", createFromLocation: 1 });
 
         //Start by getting the departments for home page
+        getDepartments();
     }
 };
 
@@ -19,7 +21,37 @@ app.initialize();
 
 //Get all departments 
 function getDepartments() {
+    myDB.transaction(function (transaction) {
+        transaction.executeSql("SELECT d.departmentId, departmentName, COUNT(e.departmentId) as total FROM department d JOIN employee e ON d.departmentId = e.departmentId GROUP BY d.departmentId ORDER BY departmentName;", [],
+            function (tx, result) {
+                //get num of rows in result
+                var dataLength = result.rows.length;
+                //check if we have any data
+                if (dataLength > 0) {
+                    //Populate listview, but first empty it
+                    $("#departmentList").empty();
+                    $("#departmentList").append('<li data-role="list-divider" data-theme="a">Departments</li>');
+                    for (var i = 0; i < dataLength; i++) {
+                        var department = result.rows.item(i);
+                        $("#departmentList").append('<li><a class="deptEmp" href="#" id="'
+                            + department.departmentId + '">'
+                            + department.departmentName +
+                            '<span class="ui-li-count">'+
+                            department.total + '</span></a></li>');
+                        // <li><a href="#" class="deptEmp" id="1">Accounting <span class="ui-li-count">1</span></a></li>
+                    }
+                    //refresh the listview widget
+                    $("#departmentList").listview().listview('refresh');                   
 
+                } else {
+                    console.log("Error getting departments.")
+                }
+
+            }, //end of success
+            function (error) {
+                console.log("Error occured while getting the data.")
+            });
+    });//end of transaction
 }
 //end getDepartments
 
