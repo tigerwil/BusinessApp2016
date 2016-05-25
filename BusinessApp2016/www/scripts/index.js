164f085e6762168e4c852cd488c0d7205efc6277
Marc Williams <oti_instructor@hotmail.com>
2016-05-25 13:17:50 -0300

    changed plugins cordova-sqlite-ext

--- BusinessApp2016/www/scripts/index.js
+++ BusinessApp2016/www/scripts/index.js
@@ -9,7 +9,7 @@
     },
     onDeviceReady: function () {
         //Open SQLite Database
-        myDB = window.sqlitePlugin.openDatabase({ name: "business.db", createFromLocation: 1 });
+        myDB = window.sqlitePlugin.openDatabase({ name: "business.db", location: 'default', createFromLocation: 1 });
 
         //Start by getting the departments for home page
         getDepartments();
@@ -57,18 +57,127 @@ function getDepartments() {
 
 //Get all employees for a specific department
 function getEmployeesByDept(departmentId) {
+    myDB.transaction(function (transaction) {
+        var sql = "SELECT id, firstName, lastName, title, picture, departmentName " +
+                  "FROM employee  JOIN department on employee.departmentId = department.departmentId " +
+                  "WHERE employee.departmentId=" + departmentId +
+                  " ORDER BY lastName;"
+        console.log(sql);
+        transaction.executeSql(sql, [],
+            function (tx, result) {
+                var dataLength = result.rows.length;
+                //console.log(dataLength);
+                if (dataLength > 0) {
+                    //start with empty listview
+                    $('#employeeList').empty();
+                    var li;
+                    for (i = 0; i < dataLength; i++) {
+                        var employees = result.rows.item(i);
+                        $('#employeeList').append('<li><a class="empdata" href="#" id="' +
+                            employees.id + '"><img src="emp_pics/' +
+                            employees.picture + '" /><h2>' +
+                            employees.title + '</h2>' +
+                            employees.firstName + ' ' + employees.lastName +
+                            '</a></li>');
+                    }
+                    $('.headinfo').html(employees.departmentName);
 
+                    //after loop is done - refresh the listview
+                    $('#employeeList').listview().listview('refresh');
+                } else {
+                    console.log("Error getting employees.");
+                }
+            },
+            function (error) {
+                console.log("Error occurred while getting the data.");
+            });
+    });
 }
 //end getEmployeesByDept
 
 //Get a single employee
 function getEmployee(id) {
+    myDB.transaction(function (transaction) {
+        var empSQL = "SELECT id, firstName, lastName, title, picture, officePhone, cellPhone, email, city " +
+                     "FROM employee " +
+                     "WHERE id=" + id;
+        //console.log(empSQL);
+        transaction.executeSql(empSQL, [],
+            function (tx, result) {
+                var dataLength = result.rows.length;
+                //alert(dataLength);
+                //console.log(dataLength);
+                if (dataLength > 0) {
+                    var employee = result.rows.item(0);
+                    //console.log(JSON.stringify(employee));
+                    $('#employeePic').attr('src', 'emp_pics/' + employee.picture);
+                    $('#fullName').text(employee.firstName + ' ' + employee.lastName);
+                    $('#employeeTitle').text(employee.title);
+                    $('#city').text(employee.city);
 
+                    //start with empty list
+                    $('#actionList').empty();
+
+                    if (employee.email) {
+                        $('#actionList').append('<li data-icon="mail"><a href="mailto:' + employee.email + '"><p class="line1">Email</p>' +
+                            '<p class="line2">' + employee.email + '</p></a></li>');
+                    }
+                    //Call office
+                    if (employee.officePhone) {
+                        $('#actionList').append('<li data-icon="user"><a href="tel:' + employee.officePhone + '"><p class="line1">Call Office</p>' +
+                            '<p class="line2">' + employee.officePhone + '</p></a></li>');
+                    }
+                    //Call Cell + Send Text msg
+                    if (employee.cellPhone) {
+                        $('#actionList').append('<li data-icon="phone"><a href="tel:' + employee.cellPhone + '"><p class="line1">Call Cell</p>' +
+                            '<p class="line2">' + employee.cellPhone + '</p></a></li>');
+                        $('#actionList').append('<li data-icon="comment"><a href="sms:' + employee.cellPhone + '"><p class="line1">SMS</p>' +
+                            '<p class="line2">' + employee.cellPhone + '</p></a></li>');
+                    }
+
+                    $('#actionList').listview().listview('refresh');
+
+                } else {
+                    console.log("Error getting employee.");
+                }
+            },
+            function (error) {
+                console.log("Error occurred while getting the data.");
+            });
+    });
 }
 //end getEmployee
 
 //================================ UI EVENT HANDLERS =======================================//
 //deptEmp Events:  (From department to employees)
+$(document).on('click', '.deptEmp', function () {
+    //alert('deptEmp')
+    $.mobile.pageContainer.pagecontainer("change", "#employees", {
+        stuff: this.id,
+        transition: "slidefade"
+    });
+});
+
+$(document).on("pagebeforechange", function (e, data) {
+    if (data.toPage[0].id === "employees") {
+        var id = data.options.stuff;
+        getEmployeesByDept(id);
+    }
+});
 
 
 //Emp details Event (For showing employee details)
+$(document).on('click', '.empdata', function () {
+    $.mobile.pageContainer.pagecontainer("change", "#employee", {
+        stuff: this.id,
+        transition: "slidefade"
+    });
+});
+
+$(document).on("pagebeforechange", function (e, data) {
+    if (data.toPage[0].id === "employee") {
+        var id = data.options.stuff;
+        //alert(id);
+        getEmployee(id);
+    }
+});
